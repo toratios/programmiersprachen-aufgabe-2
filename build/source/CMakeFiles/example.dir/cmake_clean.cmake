ed488e9bb7ed0c03172eb68c65f0c9c3c4c9d72c
toratios <max.pagel@uni-weimar.de>
2017-04-26 14:32:20 +0200

    cmake and make working

---
+++ build/source/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/window.cpp.o"
+  "CMakeFiles/example.dir/glew.c.o"
+  "CMakeFiles/example.dir/example.cpp.o"
+  "example.pdb"
+  "example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
